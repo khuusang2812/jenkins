4826ceda1070fdb0423958566bb5525f77e760e1
khuusang2812 <78064414+khuusang2812@users.noreply.github.com>
2021-06-23 00:37:08 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Printing text with newline"
-echo -n "Printing text without newline"
+echo -n "Printing text without newline sangkhuu testing"
 echo -e "\nRemoving \t backslash \t characters\n"
